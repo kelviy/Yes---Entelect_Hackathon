c704f204363a6d84c00a23ec49049ff8cb60b98e
kelviy <52874397+kelviy@users.noreply.github.com>
2024-04-13 11:46:28 +0200

    Add initalize in Plant

--- src/Plant.java
+++ src/Plant.java
@@ -10,12 +10,17 @@ public class Plant
 
     public Plant(String name,double diameter,int selling_price,Condition poor,Condition fair,Condition excellent)
     {
-        this.name = name;
-        this.diameter = diameter;
-        this.selling_price = selling_price;
-        this.poor = poor;
-        this.fair = fair;
-        this.excellent = excellent;
+        initialize(name);
+    }
+
+    public void initialize(String name) {
+        switch (name) {
+            case "Basil":
+                this.name = "Basil";
+
+        }
+
+
     }
 
     //Helper class to store the conditions for growth
