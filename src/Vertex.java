8771770ade9ca50137bc6124da30c4ca26b2bc65
kelviy <52874397+kelviy@users.noreply.github.com>
2024-04-13 14:17:55 +0200

    added check for water and sunlight

--- src/Vertex.java
+++ src/Vertex.java
@@ -46,11 +46,49 @@ public class Vertex
 
     /**
      * calculates the overall sunlight in the radius. Includes hexagons as long as they are in the circle
-     * @param radius
+     * @param diameter
      * @return the overall percentage of sunlight in the radius
      */
     public double calcSunlight(double diameter) {
         double radius = diameter / 2;
-        int distance = (int)Math.ceil(radius / 2);
+        double distance = (int)Math.ceil(radius / 2);
+
+        double total = 0;
+        double sum = 0;
+
+        Manager.garden.unweighted(cell);
+
+        for (int i = 0; i < Manager.garden.vertexMap.size(); i++) {
+            double cellDist = Manager.garden.vertexMap.get(String.valueOf(i)).dist;
+            if (cellDist <= distance) {
+                sum += Manager.garden.vertexMap.get(String.valueOf(i)).sunlight;
+                total += 100;
+            }
+        }
+        return sum/total;
+    }
+
+    /**
+     * calculates the overall sunlight in the radius. Includes hexagons as long as they are in the circle
+     * @param diameter
+     * @return the overall percentage of sunlight in the radius
+     */
+    public double calcWater(double diameter) {
+        double radius = diameter / 2;
+        double distance = (int)Math.ceil(radius / 2);
+
+        double total = 0;
+        double sum = 0;
+
+        Manager.garden.unweighted(cell);
+
+        for (int i = 0; i < Manager.garden.vertexMap.size(); i++) {
+            double cellDist = Manager.garden.vertexMap.get(String.valueOf(i)).dist;
+            if (cellDist <= distance) {
+                sum += Manager.garden.vertexMap.get(String.valueOf(i)).water;
+                total += 100;
+            }
+        }
+        return sum/total;
     }
 }
